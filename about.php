b0c56c02c2be8b1e5709e0d3622c0207c81c4658
Paul-Bel <paul.bel.job@gmail.com>
2025-02-13 14:03:59 -0500

    Ferst commit

---
+++ about.php
@@ -0,0 +1,10 @@
+<?php
+$title = "About us";
+require_once "Pages/header.php";
+?>
+
+<h1>About us</h1>
+
+<?php
+include_once "Pages/footer.php";
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$title = "Home page";
+require_once "Pages/header.php";
+?>
+
+<h1>Home Page</h1>
+
+<?php
+include_once "Pages/footer.php";
+?>
